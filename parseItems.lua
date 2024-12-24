5e627101ce9c09ab6b9a209de0bde2a9ab9248e5
Ghost <taylerobin@gmail.com>
2024-12-24 04:52:46 -0700

    idk, big rework

--- parseItems.lua
+++ parseItems.lua
@@ -1,6 +1,7 @@
 
 local fs = require("filesystem")
 local gutil = require("ghostUtils")
+local gstring = require("ghostString")
 local serial = require("serialization")
 
 local file = gutil.readFile("/item.csv"):gsub("\13", "")
@@ -10,69 +11,207 @@ if not parsedCSV then
     return
 end
 
-
+local specialFixes = {
+    modMappings = {
+        --for mods which use a different name for their identifier, and in item names. MUST be lowercase.
+        name = {
+            hungeroverhaul = "pamharvestcraft",
+            opencomputers = "oc",
+            forgemicroblock = "microblock",
+            extrautilities = "extrautils",
+            iguanatweakstconstruct = "tconstruct",
+            minefactoryreloaded = "mfr",
+            exnihilo = "crowley%.skyblock",
+            appliedenergistics = "appeng",
+        },
+        --for mods which use a different name for their identifier, and in class paths. MUST be lowercase.
+        class = {
+            awwayoftime = "alchemicalwizardry",
+            appliedenergistics = "appeng",
+            jabba = "betterbarrels",
+            opencomputers = "oc",
+            openperipheral = "openperipheral.addons",
+            extrautilities = "extrautils",
+            forgemicroblock = "microblock"
+        },
+        --for mods which use two different names in class paths. MUST be lowercase.
+        classPassTwo = {
+            gendustry = "bdew",
+        },
+    },
+    hardcoded = {
+        byName = {
+            Minecraft = {
+                mycel = "mycelium",
+                lightgem = "glowstone",
+                musicBlock = "noteBlock",
+            },
+        },
+        byClass = {
+            extracells = {
+                ItemSecureStoragePhysicalEncrypted = "SecureStorageEncrypted",
+                ItemSecureStoragePhysicalDecrypted = "SecureStorageDecrypted",
+            },
+            ExtraTrees = {
+                ItemMothDatabase = "mothDatabase"
+            }
+        },
+        byID = {        
+            HungerOverhaul = { ["105"] = "melonStem" },
+            Minecraft = {
+                ["39"] = "mushroomBrown",
+                ["40"] = "mushroomRed",
+                ["43"] = "stoneSlabDouble",
+                ["61"] = "furnaceOff",
+                ["62"] = "furnaceOn",
+                ["63"] = "signStanding",
+                ["68"] = "signWall",
+                ["70"] = "pressurePlateStone",
+                ["72"] = "pressurePlanteWood",
+                ["73"] = "redstoneOreOff",
+                ["74"] = "redstoneOreOn",
+                ["75"] = "redstoneTorchOff",
+                ["76"] = "redstoneTorchOn",
+                ["99"] = "mushroomBlockBrown",
+                ["100"] = "mushroomBlockRed",
+                ["105"] = "melonStem",
+                ["93"] = "repeaterOff",
+                ["94"] = "repeaterOn",
+                ["123"] = "redstoneLampOff",
+                ["124"] = "redstoneLampOn",
+                ["125"] = "woodSlabDouble",
+                ["149"] = "comparatorOff",
+                ["150"] = "comparatorOn",
+            }
+        }
+    }
+}
 
 local function stripMod(str, mod, modmap)
+    local modmask
     local lowerMod = mod:lower()
-    local modmask = modmap[lowerMod] or lowerMod
+    if modmap then
+        modmask = modmap[lowerMod] or lowerMod
+    else
+        modmask = mod
+    end
     local stripStr = (modmask .. "[:%.]")
-    str = gutil.stripIgnoreCase(str, stripStr, true, true)
+    str = gstring.stripIgnoreCase(str, stripStr, true, true)
     return str
 end
 -- Function to clean a string by removing all matching patterns
 local function cleanName(name, mod)
+
     local namePatterns = {
-        "^tile%.",    -- Remove "tile." at the start
-        "^block%.",   -- Remove "block." at the start
-        "^item%.",  -- Remove "item." at the start
+        "^tile[s]?%.",    -- Remove "tile." at the start
+        "^block[s]?%.",   -- Remove "block." at the start
+        "^item[s]?%.",      -- Remove "item." at the start
         " %- this item is just used to mime fluids!"
     }    
     for _, pattern in ipairs(namePatterns) do
-        name = name:gsub(pattern, "") -- Apply each pattern
+        name = gstring.stripIgnoreCase(name, pattern) -- Apply each pattern
     end
-    local modMappings = {
-        ["hungeroverhaul"] = "pamharvestcraft",
-        ["opencomputers"] = "oc",
-        ["forgemicroblock"] = "microblock",
-        ["extrautilities"] = "extrautils",
-        ["iguanatweakstconstruct"] = "tconstruct",
-        ["minefactoryreloaded"] = "mfr",
-        ["exnihilo"] = "crowley.skyblock"
+
+    local modprefixes = {
+        HardcoreQuesting = "hqm",
+        ComputerCraft = "cc",
     }
-    name = stripMod(name, mod, modMappings)
+    name = stripMod(name, mod, specialFixes.modMappings.name)
+    for _, pattern in ipairs(namePatterns) do
+        name = gstring.stripIgnoreCase(name, pattern) -- Apply each pattern
+    end
+    if modprefixes[mod] then
+        name = gstring.stripIgnoreCase(name, modprefixes[mod], true)
+    end
     name = name:gsub("%.", "_")
     return name
 end
 
 local function cleanClass(class, mod)
-    local modMappings = {
-        ["awwayoftime"] = "alchemicalwizardry",
-    }
-    class = stripMod(class, mod, modMappings)
+
+
+    class = stripMod(class, mod, specialFixes.modMappings.class)
+    if specialFixes.modMappings.classPassTwo[mod] then
+        class = stripMod(class, specialFixes.modMappings.classPassTwo[mod])
+    end
+    class = class:gsub("iguanaman.", "")
     return class
 end
 
-local function renameFromType(oldName, item)
+local function fixNameFromType(oldName, item, mod)
     return oldName .. "_" .. item.type
 end
 
-local function renameFromClass(oldName, item)
+local function fixNameFromClassCore(oldName, item, mod, tryPostfix)
     local class = item.class
-    local id = item.id
-    local tmp = gutil.extractLastSegDot(class)
-    local idk = gutil.stripIgnoreCase(tmp, oldName, true, true)
-    if idk ~= tmp then
-        tmp = oldName .. idk
-        --print(class, tmp, id)
-        os.sleep(.1)
-        return tmp
+    local newName
+    local classFinal = gstring.extractLastSegDot(class)
+    local prefix, number = oldName:match("(%a+)[_%.]?(%d+)")
+    oldName = prefix or oldName
+    if tryPostfix then
+        local classPostfix = gstring.stripIgnoreCase(classFinal, oldName, true, true)
+        if classPostfix ~= classFinal then
+            newName = oldName .. classPostfix
+            if number then
+                newName = newName .. "_" .. number
+            end
+            goto fixClassEnd
+        end
     end
-    tmp = gutil.stripIgnoreCase(tmp, "block", true)
-    tmp = gutil.stripIgnoreCase(tmp, "item", true)
-
-    --print(class, tmp)
+    newName = gstring.stripIgnoreCase(classFinal, "[Bb]lock", true)
+    newName = gstring.stripIgnoreCase(newName, "[Ii]tem", true)
+    ::fixClassEnd::
+    local modprefixes = {
+        ["AppliedEnergistics"] = "AppEng",
+        ["pamharvestcraft"] = "Pam",
+        ["ExtraTrees"] = "ET"
+    }
+    if modprefixes[mod] then
+        newName = gstring.stripIgnoreCase(newName, modprefixes[mod], true, true)
+    end
+    --print(class, newName)
     --os.sleep(.1)
-    return tmp
+    if number then
+        newName = newName .. "_" .. number
+    end
+    return newName
+end
+local function fixNameFromClass(oldName, item, mod)
+    return fixNameFromClassCore(oldName, item, mod, true)
+end
+local function fixNameFromClassPassTwo(oldName, item, mod)
+    return fixNameFromClassCore(oldName, item, mod, false)
+end
+local function hardcodedRenameEarly(name, class, mod, id)
+    class = gstring.extractLastSegDot(class)
+    local hardClass = specialFixes.hardcoded.byClass[mod]
+    local hardNames = specialFixes.hardcoded.byName
+    local hardIDs = specialFixes.hardcoded.byID
+    if hardClass and hardClass[class] then
+        name = hardClass[class]
+    end
+
+    if name == "null" then
+        local item = {class = class}
+        name = fixNameFromClassCore(name, item, mod, false)
+    end
+
+    if mod == "Minecraft" and (name == "water" or name == "lava") then
+        if class == "block.BlockFlowing" then
+            name = name .. "Flowing"
+        end
+        if class == "block.BlockStationary" then
+            name = name .. "Stationary"
+        end
+    end
+    if hardNames[mod] and hardNames[mod][name] then
+        name = hardNames[mod][name]
+    end
+
+    if hardIDs[mod] and hardIDs[mod][id] then
+        name = hardIDs[mod][id]
+    end
+    return name
 end
 
 local function fixCollisions(iTable, field, renameFunc)
@@ -96,7 +235,7 @@ local function fixCollisions(iTable, field, renameFunc)
                 if hasDifference then
                     iTable[mod][name] = nil
                     for _, item in ipairs(items) do
-                        local newName = renameFunc(name, item)
+                        local newName = renameFunc(name, item, mod)
                 
                         -- Ensure the new name exists under the mod
                         if not iTable[mod][newName] then
@@ -112,11 +251,12 @@ local function fixCollisions(iTable, field, renameFunc)
     end
 end
 local function fixColType(iTable)
-    fixCollisions(iTable, "type", renameFromType)
+    fixCollisions(iTable, "type", fixNameFromType)
 end
 
 local function fixColClass(iTable)
-    fixCollisions(iTable, "class", renameFromClass)
+    fixCollisions(iTable, "class", fixNameFromClass)
+    fixCollisions(iTable, "class", fixNameFromClassPassTwo)
 end
 
 local itemTable = {}
@@ -134,11 +274,7 @@ for i, row in ipairs(parsedCSV) do
         end
         local parsedName = cleanName(unlocalised, mod)
         local parsedClass = cleanClass(class, mod)
-
-        if parsedName == "null" then
-            parsedName = gutil.extractLastSegDot(parsedClass)
-        end
-
+        parsedName = hardcodedRenameEarly(parsedName, parsedClass, mod, id)
 
         -- Ensure the mod key exists in the nestedTable
         if not itemTable[mod] then
@@ -162,10 +298,10 @@ end
 
 fixColType(itemTable)
 fixColClass(itemTable)
-serfile = serial.serialize(itemTable)
-serfile = serfile:gsub("}}},", "}}},\n")
-serfile = serfile:gsub("}},", "}},\n")
-local output = "itemTable = " .. serfile .. "\n" .. "return itemTable"
+fixColType(itemTable)
+local serfile = serial.serialize(itemTable)
+serfile = serfile:gsub("}}},", "}}},\n"):gsub("}},", "}},\n")
+local output = "local itemTable = " .. "\n\n" .. serfile .. "\n\n" .. "return itemTable"
 gutil.strToFile("/itemTable.lua", output)
 
 print("Item Table generated to \"/itemTable.lua\"")
